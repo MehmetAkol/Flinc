462f99f858d580f0af97f175ea314e67547e0259
Zikria Azimi <zikria.azimi@hotmail.com>
2017-12-15 13:59:53 +0100

    C1 Zikria

---
+++ application/views/application.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel = "stylesheet" type = "text/css"
+          href = "<?php echo base_url(); ?>css/style.css">
+
+    <!-- Bootstrap -->
+    <link rel = "stylesheet" type = "text/css"
+          href = "<?php echo base_url(); ?>css/bootstrap.css">
+    <script src = "<?php echo base_url(); ?>js/bootstrap.js"></script>
+    <script src = "<?php echo base_url(); ?>js/jquery-3.2.1.js"></script>
+
+</head>
+<body>
+<?php include_once "templates/loginPage/loginTemplate.php"; ?>
+
+</body>
+</html>
---
+++ application/views/templates/loginPage/loginTemplate.php
@@ -0,0 +1,13 @@
+<div class="container">
+    <div class="form-group">
+        <label for="username">Username</label>
+        <input type="text" placeholder="" class="form-control" id="username" placeholder="Your username" />
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" placeholder="" class="form-control" id="password" placeholder="Your password" />
+    </div>
+    <div class="btn-group-vertical btn-block text-center" role="group">
+        <button class="btn btn-danger btn-lg">Login Now</button>
+        <button class="btn btn-danger btn-lg">Forgot Password</button>
+    </div>
